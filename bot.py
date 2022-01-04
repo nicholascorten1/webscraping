5d6e5e74fb769c6920ecf437ed3e184735ba2893
Maarten van Meeuwen <maartenvanmeeuwen@gmail.com>
2022-01-04 10:27:47 +0100

    framework for setting users

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 from config import set_chrome_options
-
+from utils.set_user import set_user
 
 
 base_url = "https://shop.datasight.app"
@@ -14,9 +14,21 @@ base_url = "https://shop.datasight.app"
 
 def main(): 
 
+    
+    
+
+
+    
     driver = webdriver.Chrome(ChromeDriverManager().install(), options = set_chrome_options())
     response = driver.get(base_url)
     
+    ## Set user: 
+    user = set_user() 
+    driver.delete_cookie('_ga')
+    driver.add_cookie({
+        'name': '_ga',
+        'value': user
+    })
 
     driver.close()
 
---
+++ utils/set_user.py
@@ -0,0 +1,19 @@
+import random
+
+def set_user() -> str:
+    """[summary]
+    generates random slur from a fixed set of user for user compliant with ga analytics conventions
+    """
+
+    ## TODO --> create from a fixed list of users or set a seed to generate users corresponding to users in segmentss
+    nr_of_customers = 1000
+    seed_int = random.randint(0, nr_of_customers)
+
+    left_part = random.randint(1e9, 1e10-1)
+    
+
+    right_part = random.randint(1e9, 1e10-1)
+    user = f"{left_part}.{right_part}"
+    
+    return 'GA1.1.' + user
+    
