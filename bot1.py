138189f9f42d9b32a0660d80d34c154457ee1333
Nicholas Corten <nicholasl.corten@agiliz.be>
2022-01-05 11:33:01 +0100

    bot opens website, shop and item category

--- bot1.py
+++ bot1.py
@@ -1,7 +1,23 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 PATH = "/usr/local/bin/chromedriver"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://shop.datasight.app")
 
+link = driver.find_element_by_link_text("Shop")
+link.click()
+
+try:
+    element = WebDriverWait(driver, 5).until(
+        EC.presence_of_all_elements_located((By.LINK_TEXT, "Uncategorized"))
+    )
+    element.click()
+except:
+    driver.quit()
+#driver.quit()
