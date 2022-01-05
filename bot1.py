f90453f9a70392ce6f25ca19ae484b8c69c1066e
Nicholas Corten <nicholasl.corten@agiliz.be>
2022-01-05 16:01:28 +0100

    bot added cola to cart

--- bot1.py
+++ bot1.py
@@ -13,11 +13,16 @@ driver.get("https://shop.datasight.app")
 link = driver.find_element_by_link_text("Shop")
 link.click()
 
+# waiting up to 10 seconds for the driver to find the element that is called "beverages"
 try:
-    element = WebDriverWait(driver, 5).until(
-        EC.presence_of_all_elements_located((By.LINK_TEXT, "Uncategorized"))
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//img[@alt='beverages']"))
+    )
+    element.click()
+
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Add to cart"))
     )
     element.click()
 except:
     driver.quit()
-#driver.quit()
