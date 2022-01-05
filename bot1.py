68609dba51f4272130de24f1a5c0eae4a293d0cd
Nicholas Corten <nicholasl.corten@agiliz.be>
2022-01-05 16:14:13 +0100

    bot viewed cart

--- bot1.py
+++ bot1.py
@@ -24,5 +24,10 @@ try:
         EC.presence_of_element_located((By.LINK_TEXT, "Add to cart"))
     )
     element.click()
+
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//a[@title='View cart']"))
+    )
+    element.click()
 except:
     driver.quit()
