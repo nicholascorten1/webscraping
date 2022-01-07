fa45cb397ee74fb3c43ccc67c35b8674dc023fa9
Nicholas Corten <nicholasl.corten@agiliz.be>
2022-01-07 09:33:00 +0100

    functions in bot.py

--- bot.py
+++ bot.py
@@ -2,25 +2,20 @@
     Bot for browsing shop.datasight.app
 """
 
+from datetime import time
 from selenium import webdriver 
 from webdriver_manager.chrome import ChromeDriverManager
 
 from config import set_chrome_options
 from utils.set_user import set_user
+from commands import open_website, open_shop, choose_beverages, pick_drink, add_to_cart, view_cart, proceed_checkout, fill_firstname, fill_lastname, pick_country, fill_street_address, fill_city, fill_state, fill_postcode, fill_phonenumber, fill_email
 
 
-base_url = "https://shop.datasight.app"
+
 
 
 def main(): 
-
-    
-    
-
-
-    
     driver = webdriver.Chrome(ChromeDriverManager().install(), options = set_chrome_options())
-    response = driver.get(base_url)
     
     ## Set user: 
     user = set_user() 
@@ -29,6 +24,26 @@ def main():
         'name': '_ga',
         'value': user
     })
+    open_website()
+    open_shop()
+    choose_beverages()
+    pick_drink()
+    add_to_cart()
+    view_cart()
+    proceed_checkout()
+    fill_firstname()
+    fill_lastname()
+    pick_country()
+    fill_street_address()
+    fill_city()
+    fill_state()
+    fill_postcode()
+    fill_phonenumber()
+    fill_email() 
+    
+   
+
+
 
     driver.close()
 
