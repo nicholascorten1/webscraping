469b5f991aaad1d27bace33ca0c91b1149784756
Nicholas Corten <nicholasl.corten@agiliz.be>
2022-01-11 17:14:56 +0100

    in case of fire haha

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@
     Bot for browsing shop.datasight.app
 """
 
-from datetime import time
 from selenium import webdriver 
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -16,26 +15,19 @@ from utils.set_user import set_user
 
 def main(): 
     driver = webdriver.Chrome(ChromeDriverManager().install(), options = set_chrome_options())
-    
-    ## Set user: 
-    user = set_user() 
-    driver.delete_cookie('_ga')
-    driver.add_cookie({
+    i = 0
+    while(i < 5):
+
+        ## Set user: 
+        user = set_user() 
+        driver.delete_cookie('_ga')
+        driver.add_cookie({
         'name': '_ga',
         'value': user
-    })
-    
-    if segment == "single_shopper"
-        single_shopper_bevahiour(driver)
+        })
+        loyal_customer_behaviour()
+        i = i + 1
 
-    if segment == "window_shopper"
-        window_shopper_bevahiour(driver)
-    
-    if segment == "researcher"
-        researcher_bevahiour(driver)
-    
-    if segment == "loyal_customer"
-        loyal_customer_behaviour(driver)
 
 
 if __name__ == "__main__":
