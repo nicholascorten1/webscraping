70cf71044b9d055cd0eec2c55a46f55962327607
Nicholas Corten <nicholasl.corten@agiliz.be>
2022-01-14 13:29:55 +0100

    made a loop to iterate over the different types of segments in a list of users

--- utils/set_user.py
+++ utils/set_user.py
@@ -1,3 +1,4 @@
+from segments import *
 import random
 random.seed(1)
 
@@ -17,7 +18,7 @@ def set_user() -> str:
     """
     left_part = random.randint(1e9, 1e10-1)
     right_part = random.randint(1e8, 1e9-1)
-    segment = random.randint(1,5)
+    segment = random.randrange(1,5)
 
     user = f"{left_part}.{right_part}{segment}"
     print('user--', user)
@@ -35,5 +36,22 @@ for i in range(amount_users):
     ga_id = set_user()
     list_of_users.append(ga_id)
 
-print(list_of_users)
-print(list_of_users)
+
+"""
+list_of_users consists of the cookie for every user. the last number of every cookie is the segment the user belongs to.
+so the if statement executes the website journey that matches for that type of user
+"""
+
+for j in range(len(list_of_users)):
+    segment_type = eval(list_of_users[j][-1])
+    if segment_type == 1:
+        single_shopper_bevahiour()
+
+    elif segment_type == 2:
+        window_shopper_behaviour()
+
+    elif segment_type == 3:
+        researcher_behaviour()
+
+    else:
+        loyal_customer_behaviour()
